b374654fe68b7e788c73f7feaffe8f93b8485682
atrasnajwan <atrasnajwan@gmail.com>
2024-06-08 00:57:50 +0700

    add logging middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+const morgan = require('morgan')
 const planetRoutes = require('./routes/planets/planets.router')
 
 const app = express()
@@ -8,6 +9,9 @@ const app = express()
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
+
+// logging
+app.use(morgan('combined'))
 
 // serve static files from builded react app
 app.use(express.static(path.join(__dirname, "..", "public")))
