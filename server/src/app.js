b75bbd23680bb6d525edfd7cf683d7612447998a
atrasnajwan <atrasnajwan@gmail.com>
2024-06-08 00:45:56 +0700

    add deploy script

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const planetRoutes = require('./routes/planets/planets.router')
 
 const app = express()
@@ -7,6 +8,11 @@ const app = express()
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
+
+// serve static files from builded react app
+app.use(express.static(path.join(__dirname, "..", "public")))
+
+
 app.use(express.json())
 app.use(planetRoutes)
 
