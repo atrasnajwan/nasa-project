2186cf29edb87c2f5ffab34032c0ed0d917f6315
atrasnajwan <atrasnajwan@gmail.com>
2025-04-30 16:57:29 +0700

    change create and get planet to db

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,39 +1,48 @@
 const fs = require('fs')
 const path = require('path')
 const { parse } = require('csv-parse')
+const Planet = require('./planets.mongo')
 
-const planetsCsv = path.join(__dirname, '..', '..', 'data', 'kepler_data.csv') 
-const habitablePlanets = []
+const planetsCsv = path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
 
 const isHabitable = (planet) => {
     return planet['koi_disposition'] === 'CONFIRMED'
-            && planet['koi_insol'] > 0.36
-            && planet['koi_insol'] < 1.11
-            && planet['koi_prad'] < 1.6
+        && planet['koi_insol'] > 0.36
+        && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6
 }
 
 const loadPlanets = () => {
     return new Promise((resolve, reject) => {
         fs.createReadStream(planetsCsv)
-        .pipe(parse({
-            comment: '#',
-            columns: true
-        }))
-        .on('data', (data) => {
-            if(isHabitable(data)) habitablePlanets.push(data)
-        })
-        .on('error', (error) => {
-            console.log('ERROR: ', error)
-            reject(error)
-        })
-        .on('end', () => {
-            console.log("Planets succesfully load")
-            resolve()
-        })
+            .pipe(parse({
+                comment: '#',
+                columns: true
+            }))
+            .on('data', async (data) => {
+                if (isHabitable(data)) await updatePlanet(data)
+            })
+            .on('error', (error) => {
+                console.log('ERROR: ', error)
+                reject(error)
+            })
+            .on('end', async () => {
+                const count = (await getAllPlanets()).length
+                console.log(`${count} planets succesfully load`)
+                resolve()
+            })
     })
 }
 
-const getAllPlanets = () => habitablePlanets
+const updatePlanet = async (planet) => await Planet.updateOne({
+    keplerName: planet.kepler_name // WHERE
+}, {
+    keplerName: planet.kepler_name // ACTION
+}, {
+    upsert: true    // UPSERT
+})
+
+const getAllPlanets = async () => await Planet.find({})
 
 module.exports = {
     loadPlanets,
--- server/src/server.js
+++ server/src/server.js
@@ -2,14 +2,16 @@ const http = require('http')
 const app = require('./app')
 const mongoose = require('mongoose')
 const { loadPlanets } = require('./models/planets.model')
-const { log, error } = require('console')
 
 const PORT = process.env.PORT || 8000
 const server = http.createServer(app)
 const MONGO_PASSWORD =  process.env.MONGO_PASSWORD 
 const MONGO_APP =  process.env.MONGO_APP || "nasa-cluster"
-const MONGO_URL = `mongodb+srv://nasa-api:${MONGO_PASSWORD}@nasa-cluster.adkkiow.mongodb.net/?retryWrites=true&w=majority&appName=${MONGO_APP}`
+// const MONGO_URL = `mongodb://nasa-api:${MONGO_PASSWORD}@nasa-cluster.adkkiow.mongodb.net/?retryWrites=true&w=majority&appName=${MONGO_APP}`
+// used older connection 
+const MONGO_URL = `mongodb://nasa-api:${MONGO_PASSWORD}@ac-kesvamv-shard-00-00.adkkiow.mongodb.net:27017,ac-kesvamv-shard-00-01.adkkiow.mongodb.net:27017,ac-kesvamv-shard-00-02.adkkiow.mongodb.net:27017/?ssl=true&replicaSet=atlas-os23kt-shard-0&authSource=admin&retryWrites=true&w=majority&appName=${MONGO_APP}`
 
+// event emitter on mongoose
 mongoose.connection.on("open", () => {
     console.log("MongoDB connect success")
 })
@@ -20,6 +22,7 @@ mongoose.connection.on("error", (error) => {
 
 const startServer = async () => {
     // connect to db
+    console.log("Connecting to db...")
     await mongoose.connect(MONGO_URL)
     // Load data
     await loadPlanets()
