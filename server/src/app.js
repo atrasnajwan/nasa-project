e75fb97fa2946bd759bc34459166df965b9ce095
atrasnajwan <atrasnajwan@gmail.com>
2025-05-01 19:55:29 +0700

    and dotenv for environment variable and move routes to api.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,8 @@ const express = require('express')
 const cors = require('cors')
 const path = require('path')
 const morgan = require('morgan')
-const planetsRouter = require('./routes/planets/planets.router')
-const launchesRouter = require('./routes/launches/launches.router')
+const api = require('./routes/api')
+
 
 const app = express()
 
@@ -18,8 +18,8 @@ app.use(morgan('combined'))
 app.use(express.static(path.join(__dirname, "..", "public")))
 
 app.use(express.json())
-app.use('/planets', planetsRouter)
-app.use('/launches', launchesRouter)
+// API
+app.use('/', api)
 
 // fix routes on production mode
 // https://create-react-app.dev/docs/deployment/
